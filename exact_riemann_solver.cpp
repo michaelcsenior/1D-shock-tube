b5c93b8f79ee4d0cda61fe013eeb6642fa2ee3dd
michaelcsenior <220513597+michaelcsenior@users.noreply.github.com>
2026-06-02 20:06:19 -0400

    no message

--- exact_riemann_solver.cpp
+++ exact_riemann_solver.cpp
@@ -1,3 +1,17 @@
 // created 2026-06-02 by Michael
 
+#pragma region libraries
+#include <iostream>
+#include <string>
 
+#include <Eigen/Core>
+#pragma endregion
+
+#pragma region classes
+
+#pragma endregion
+
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
